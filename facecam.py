14668884e5d673431a882925ed275e1503ae943c
zidanfakhrezi12 <zidanfakhrezi666@gmail.com>
2023-05-12 10:20:50 +0700

    1
    test

--- facecam.py
+++ facecam.py
@@ -15,7 +15,7 @@ while True:
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
 	for (x, y, w, h) in faces:
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 			0),2)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0),2)
 		roi_gray = gray[y : y + h, x : x + w]
 		roi_color = frame[y : y + h, x : x + w]
 		eyes = eye_cascade.detectMultiScale(roi_gray)
